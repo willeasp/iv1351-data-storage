86e44e4ce1f01c6407ea48945625b13a684b27f2
William Asp <wille.asp@live.se>
2020-12-22 11:21:22 +0100

    queries 1-5 done

--- PopulateData.sql
+++ PopulateData.sql
@@ -81,18 +81,51 @@ INSERT INTO person (person_number,
                 '4',
                 '34567',
                 'Wien'
+            ), (
+                '191901018888',
+                'Thomas',
+                'Ravelli',
+                '0708862312',
+                'ravelli@mail.com',
+                'Fotbollsgatan',
+                '1',
+                '12387',
+                'Madrid'
+            ), (
+                '2001-12-18',
+                'Billie',
+                'Eilish',
+                '0731217459',
+                'avocado@where.com',
+                'BeverlyHillsStreet',
+                '69',
+                '66679',
+                'Los Angeles'
+            ), (
+                '198903237777',
+                'Felix',
+                'Kjellberg',
+                '0724562382',
+                'pewdiepie@gmail.com',
+                'Barrelstreet',
+                '420',
+                '33497',
+                'Brighton'
             );
 
 
 /* Insert 3 students */
 INSERT INTO student (person_id)
-            VALUES (1),
+            VALUES  (1),
                     (2),
-                    (3);
+                    (3),
+                    (7),
+                    (8),
+                    (9);
 
 /* Insert 3 instructors */
 INSERT INTO instructor (person_id, teaches_ensembles)
-            VALUES (4, true),
+            VALUES  (4, true),
                     (5, false),
                     (6, true);
 
@@ -122,6 +155,7 @@ INSERT INTO rental (start_date, end_date, student_id, ri_id)
                     ('2020-03-01', '2020-04-01', 2, 6),
                     ('2020-04-01', '2020-05-01', 3, 7),
                     ('2020-04-01', '2020-05-01', 3, 8),
+                    ('2020-05-01', '2020-06-01', 3, 9),
                     ('2020-05-01', '2020-06-01', 3, 9);
 
 
@@ -136,6 +170,7 @@ INSERT INTO instrument (name, type)
 INSERT INTO individual_lesson (instrument_id, level, instructor_id, student_id, start_time, end_time, date, price, pay)
             VALUES  (1, 'beginner', 1, 1, '10:00', '11:00', '2020-12-10', 150, 120),
                     (2, 'intermediate', 2, 2, '11:00', '12:00', '2020-04-13', 150, 120),
+                    (2, 'intermediate', 2, 2, '17:00', '18:00', '2020-04-15', 150, 120),
                     (3, 'advanced', 3, 3, '09:00', '11:00', '2020-12-15', 180, 150);
 
 INSERT INTO group_lesson (instrument_id, level, instructor_id, max_students, min_students, start_time, end_time, date, price, pay)
@@ -143,10 +178,32 @@ INSERT INTO group_lesson (instrument_id, level, instructor_id, max_students, min
                     (2, 'intermediate', 2, 20, 8, '15:00', '17:00', '2020-11-20', 100, 120),
                     (3, 'advanced', 3, 8, 3, '13:00', '15:30', '2020-11-10', 120, 150);
 
-INSERT INTO ensemble (genre, max_students, min_students, level, instructor_id, start_time, end_time, date, price, pay)
-            VALUES  ('afrobeat', 30, 10, 'beginner', 1, '09:00', '12:00', '2020-06-13', 120, 150),
-                    ('post-dubstep', 30, 10, 'intermediate', 1, '10:00', '12:00', '2020-10-14', 120, 150),
-                    ('metalcore', 30, 10, 'advanced', 3, '14:00', '16:00', '2020-10-15', 150, 200);
+INSERT INTO ensemble(genre, max_students, min_students, level, instructor_id, start_time, end_time, date, price, pay)
+            VALUES  ('afrobeat',        5,  2, 'beginner',     1, '09:00', '12:00', '2020-06-13', 120, 150),
+                    ('post-dubstep',    5,  2, 'intermediate', 1, '10:00', '12:00', '2020-10-14', 120, 150),
+                    ('metalcore',       5,  2, 'advanced',     3, '14:00', '16:00', '2020-10-15', 150, 200),
+                    ('afrobeat',        5,  2, 'beginner',     1, '09:00', '12:00', '2020-12-13', 120, 150),
+                    ('post-dubstep',    5,  2, 'intermediate', 2, '10:00', '12:00', '2020-12-14', 120, 150),
+                    ('metalcore',       5,  2, 'advanced',     3, '14:00', '16:00', '2020-12-15', 150, 200);
 
+
+/* Insert students into ensembles */
+INSERT INTO ensemble_lesson_student(student_id, ensemble_id)
+            VALUES  (1, 1), -- 3 students to ensemble 1
+                    (2, 1),
+                    (3, 1),
+                    (1, 2), -- 5 students to ensemble 2
+                    (2, 2),
+                    (3, 2),
+                    (4, 2),
+                    (5, 2),
+                    (6, 3), -- 4 students to ensemble 3
+                    (5, 3),
+                    (4, 3),
+                    (3, 3),
+                    (6, 4), -- 2 students to ensemble 4
+                    (5, 4),
+                    (6, 5); -- 1 student to ensemble 5
+                    -- 0 students to ensemble 6
 
 COMMIT;
--- task3.sql
+++ task3.sql
@@ -1,22 +1,22 @@
 
 
 /* 
-1. NOT FINISHED
+1. FINISHED
 Show the number of instruments rented per month during a specified year. 
 It shall be possible to retrieve the total number of rented instruments 
 (just one number) and rented instruments of each kind (one number per kind, 
-guitar, trumpet, etc). The latter list shall be sorted by number of rentals. 
+guitar, trumpet, etc). The latter list shall be sorted BY number of rentals. 
 This query is expected to be performed a few times per week. 
 */
-/* 
-SELECT  COALESCE("Month", 'ALL') AS "Month", 
-        COALESCE("Instrument", 'TOTAL') AS "Instrument",
-        "Rentals" 
+
+SELECT  COALESCE("month", 'ALL') AS month, 
+        COALESCE("instrument", 'TOTAL') AS instrument,
+        rentals 
 FROM    (
     SELECT  
-        TO_CHAR(r.start_date, 'Month') AS "Month", 
-        ri.name AS "Instrument", 
-        COUNT(ri.name) AS "Rentals" 
+        TO_CHAR(r.start_date, 'Month') AS month, 
+        ri.name AS instrument, 
+        COUNT(ri.name) AS rentals
     FROM 
         rental AS r
     JOIN 
@@ -26,129 +26,101 @@ FROM    (
     WHERE 
         EXTRACT(YEAR FROM r.start_date) = 2020
     GROUP BY 
-        ROLLUP("Month"), ROLLUP("Instrument")
+        ROLLUP(month), ROLLUP(instrument)
 ) AS query
 ORDER BY 
-    "Month"; */
+    rentals DESC;
 
 /* 
    Month   | Instrument | Rentals
 -----------+------------+---------
- ALL       | TOTAL      |       9
- ALL       | violin     |       3
- ALL       | trumpet    |       3
- ALL       | guitar     |       3
- April     | TOTAL      |       2
- April     | trumpet    |       2
- February  | TOTAL      |       2
- February  | guitar     |       1
  February  | violin     |       1
+ May       | trumpet    |       1
+ May       | TOTAL      |       1
+ February  | guitar     |       1
+ April     | TOTAL      |       2
+ March     | TOTAL      |       2
+ February  | TOTAL      |       2
  January   | TOTAL      |       2
  January   | guitar     |       2
  March     | violin     |       2
- March     | TOTAL      |       2
- May       | TOTAL      |       1
- May       | trumpet    |       1 */
+ April     | trumpet    |       2
+ ALL       | guitar     |       3
+ ALL       | violin     |       3
+ ALL       | trumpet    |       3
+ ALL       | TOTAL      |       9*/
 /********************************************************************************************************************/
 
 /* 
-2. PROBABLY FINISHED
+2. FINISHED
 The same as above, but retrieve the average number of rentals per month 
 during the entire year, instead of the total for each month. 
 */
 
--- Average rentals of each instrument per month
-/* SELECT 
-    "Instrument",
-    CAST(AVG("Rentals") AS DECIMAL(10, 2)) AS "Average"
-FROM (
-    SELECT 
-        TO_CHAR(r.start_date,'Mon') AS "Month",
-        ri.name AS "Instrument",
-        COUNT(*) AS "Rentals"
-        FROM 
-            rental AS r
-        JOIN 
-            rental_instrument AS ri
-        ON 
-            r.ri_id = ri.ri_id
-        WHERE
-            EXTRACT(YEAR FROM start_date) = 2020
-        GROUP BY 
-            "Month", "Instrument"
-    UNION
-    SELECT 
-        TO_CHAR(date '1970-01-01' + interval '1' month * s.a, 'Mon') AS month,
-        '' "Intrument",
-        0 "Rentals"
-        FROM generate_series(0,11,1) AS s(a)
-) AS rentals
-GROUP BY    
-    "Instrument" */
-
 SELECT 
-        TO_CHAR(date '1970-01-01' + interval '1' month * s.a, 'Mon') AS month,
-        instrument
-        FROM generate_series(0,11,1) AS s(a)
-        inner join lateral (select name from instrument) as q
-        ;
-
-/* UNION ALL
--- Get average of all instruments across all months
-SELECT
-    'ALL' AS "Instrument",
-    CAST(AVG("Rented") AS DECIMAL(10, 2)) AS "Average" 
+    COALESCE("instrument", 'ALL') AS instrument,
+    CAST(COUNT(*) /12.0 AS DECIMAL(10,2)) AS avg_rentals
 FROM (
     SELECT 
-        TO_CHAR(start_date,'Mon') AS "Month",
-        COUNT(*) AS "Rented"
-    FROM 
-        rental
-    WHERE
-        EXTRACT(YEAR FROM start_date) = 2020
-    GROUP BY
-        "Month" 
-) AS rentals */;
-
-/* OUTPUT
- Instrument | Average
-------------+---------
- guitar     |    1.50 detta är fan konstigt du
- trumpet    |    1.50
- violin     |    1.50
- ALL        |    1.80
-  */
+        TO_CHAR(r.start_date, 'Month') AS month,
+        ri.name AS instrument
+    FROM    
+        rental_instrument AS ri
+    INNER JOIN  
+        rental AS r
+    ON r.ri_id = ri.ri_id
+    ) AS rentals
+GROUP BY 
+    ROLLUP(instrument)
+ORDER BY
+    avg_rentals;
 
 
 /********************************************************************************************************************/
 
 /* 
-3. NOT FINISHED
+3. FINISHED
 Show the number of lessons given per month during a specified year. It shall 
 be possible to retrieve the total number of lessons (just one number) and 
 the specific number of individual lessons, group lessons and ensembles. This 
 query is expected to be performed a few times per week. 
 */
 
-/* select  TO_CHAR(date, 'Month') AS month, 
-        sum(i) i_lessons, 
-        sum(g) g_lessons, 
-        sum(e) ensembles, 
-        sum(t) total
-    from (
-        select date, i, g, e, t
-            from (select date, 1 i, 0 g, 0 e, 1 t
-                from individual_lesson) a1
-                union 
-                (select date m, 0 i, 1 g, 0 e, 1 t
-                from group_lesson )
-                union 
-                (select date m, 0 i, 0 g, 1 e, 1 t
-                from ensemble ) ) a2
-    where 
-        EXTRACT(YEAR FROM date) = 2020
-    group by month 
-    order by month; */
+
+SELECT  COALESCE("month", 'ALL') AS month,
+        SUM(i) i_lessons, 
+        SUM(g) g_lessons, 
+        SUM(e) ensembles, 
+        SUM(t) total
+FROM (
+    SELECT 
+        EXTRACT(YEAR FROM date) AS year,
+        TO_CHAR(date, 'Month') AS month,
+        i, g, e, t
+    FROM (
+        SELECT 
+            date, 1 i, 0 g, 0 e, 1 t
+        FROM 
+            individual_lesson
+        UNION (
+            SELECT 
+                date m, 0 i, 1 g, 0 e, 1 t
+            FROM 
+                group_lesson 
+        )
+        UNION (
+            SELECT 
+                date m, 0 i, 0 g, 1 e, 1 t
+            FROM 
+                ensemble 
+        )
+    ) a1
+) a2
+WHERE 
+    year = 2020 
+
+GROUP BY ROLLUP(month)
+ORDER BY total DESC;
 
 /* 
    month   | i_lessons | g_lessons | ensembles | total
@@ -161,17 +133,114 @@ query is expected to be performed a few times per week.
 /********************************************************************************************************************/
 
 /* 
-4. NOT FINISHED
+4. FINISHED
 The same as above, but retrieve the average number of lessons per month
 during the entire year, instead of the total for each month. */
+
+SELECT  EXTRACT(YEAR FROM date) AS year, 
+        CAST(SUM(i) /12.0 AS DECIMAL(10,2)) AS i_lessons, 
+        CAST(SUM(g) /12.0 AS DECIMAL(10,2)) AS g_lessons, 
+        CAST(SUM(e) /12.0 AS DECIMAL(10,2)) AS ensembles, 
+        CAST(SUM(t) /12.0 AS DECIMAL(10,2)) AS total
+    FROM (
+        SELECT 
+            date, i, g, e, t
+            FROM (
+                SELECT 
+                    date, 1 i, 0 g, 0 e, 1 t
+                FROM 
+                    individual_lesson
+                ) a1
+                UNION (
+                SELECT 
+                    date m, 0 i, 1 g, 0 e, 1 t
+                FROM 
+                    group_lesson 
+                )
+                UNION (
+                SELECT 
+                    date m, 0 i, 0 g, 1 e, 1 t
+                FROM 
+                    ensemble 
+                ) 
+        ) a2
+    WHERE 
+        EXTRACT(YEAR FROM date) = 2020
+    GROUP BY year
+    ORDER BY year;
 
 
 /********************************************************************************************************************/
 
 /* 
-5. NOT FINISHED
+5. YÄÄ it's probably FINISHED
 List all instructors who has given more than a specific number of lessons
-during the current month. Sum all lessons, independent of type. Also list the 
+during the current month. SUM all lessons, independent of type. Also list the 
 three instructors having given most lessons (independent of lesson type) during 
-the last month, sorted by number of given lessons. This query will be used to 
+the last month, sorted BY number of given lessons. This query will be used to 
 find instructors risking to work too much, and will be executed daily. */
+
+
+-- Get the 3 instructors with the most amount of lessons
+(SELECT
+    instructor_id AS instructor,
+    COUNT(instructor_id) AS given_lessons
+FROM (
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        individual_lesson
+    UNION
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        group_lesson
+    UNION
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        ensemble
+) AS lessons
+WHERE
+    date >= DATE_TRUNC('month', CURRENT_DATE)
+GROUP BY 
+    instructor
+ORDER BY 
+    given_lessons DESC
+LIMIT 3)
+-- together with
+UNION
+-- Get the isntructors that have done at least 2 lessons
+SELECT 
+    instructor_id AS instructor,
+    COUNT(instructor_id) AS given_lessons
+FROM (
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        individual_lesson
+    UNION
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        group_lesson
+    UNION
+    SELECT 
+        date, 
+        instructor_id 
+    FROM 
+        ensemble
+) AS lessons
+WHERE
+    date >= DATE_TRUNC('month', CURRENT_DATE)
+GROUP BY
+    instructor
+HAVING
+    COUNT(instructor_id) >= 2
+ORDER BY given_lessons DESC;
+
